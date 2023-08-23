70dccaa4453f16c6971606eff80f21116ae2cd40
alevdenizakca <alevdenizakca@gmail.com>
2023-08-23 19:40:51 +0300

    ServiceLayer edited

--- NLayer.Repository/Repositories/GenericRepository.cs
+++ NLayer.Repository/Repositories/GenericRepository.cs
@@ -30,7 +30,7 @@ namespace NLayer.Repository.Repositories
             return await _dbSet.AnyAsync(expression);
         }
 
-        public IQueryable<T> GetAll(Expression<Func<T, bool>> expression)
+        public IQueryable<T> GetAll()
         {
             return _dbSet.AsNoTracking().AsQueryable(); //AsNoTrack kullanma sebebi çekilen dataların memory'e alınıp takip edilmesini önlemek ve böylece programın daha performanslı çalışması
         }
